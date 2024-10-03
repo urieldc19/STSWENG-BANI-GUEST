05000e573deca8d14e626f00cc0a6293c0ad9ae1
joehannaC <jhnnacansino@gmail.com>
2024-10-03 21:10:35 +0800

    Update tailwind.config.js
    added font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,6 +11,7 @@ export default {
         },
         fontFamily:{
           'serif': ['Inria Serif', 'sans-serif'],
+          'JimNightshade': ['"Jim Nightshade"', 'cursive'],
         },
       },
     },
