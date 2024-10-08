fb0a618ef1c06585da19e0d6d4110a9ecc38c334
joehannaC <jhnnacansino@gmail.com>
2024-10-08 19:49:07 +0800

    Update SpecialOffers.tsx
    changed wedding packages

--- client/src/pages/SpecialOffers.tsx
+++ client/src/pages/SpecialOffers.tsx
@@ -1,6 +1,7 @@
 import ReserveNowButton from "../components/ReserveButton";
-import SpecialOffersBox from "../components/SpecialOffersBox";
 import { Slideshow } from "../components/Slideshow";
+import SpecialOffersBox from "../components/SpecialOffersBox";
+import WeddingPackagesCarousel from "../components/WeddingPackages";
 
 export function SpecialOffers () {
 
@@ -25,111 +26,17 @@ export function SpecialOffers () {
         <div className="my-6 text-center">
             <h2 className="mb-2 font-serif text-4xl text-black">Make your Wedding most memorable</h2>
                 <div className="w-5/6 mx-auto mb-8 border-b-2 border-black"></div> 
+                <p className="font-serif text-lg text-justify text-black ml-28 mr-28">At Bani Hidden Paradise Beach Resort, we believe that your special day deserves to be nothing less than extraordinary. Imagine exchanging vows with the sound of waves gently lapping at the shore, surrounded by breathtaking views and lush tropical landscapes. Our resort offers the perfect blend of romance, beauty, and luxury, ensuring that every moment of your celebration is unforgettable.
+                    From intimate ceremonies to grand receptions, our dedicated team will work closely with you to customize every detail, from exquisite floral arrangements to gourmet catering that tantalizes the taste buds. Let us help you create cherished memories that you and your loved ones will treasure forever.
+                    Your dream wedding awaits—let's make it a reality together!</p>
         </div>
     
 
-        <div className="z-20 flex space-x-5">
-
-             {/* Packages */}
-                <div className="grid grid-cols-1 gap-6 px-8 md:grid-cols-3">
-                
-                {/* Wedding Package A */}
-                <div className="mt-5 mb-5 ml-5 w-100 p-6 text-gray-800 transition-colors duration-200 inner-shadow bg-[#A0B1B5] opacity-75 hover:bg-[#2F515B] hover:text-[#F2EFE8]">
-                    {/* Adjusted font size for package titles */}
-                    <h3 className="mb-4 font-serif text-3xl font-semibold">Wedding Package A</h3>
-                    <ul className="mt-4 mb-4 font-serif text-lg font-semibold">
-                    <p>Rate: 500,000</p>
-                    <p>Promo Rate: 450,000</p>
-                    </ul>
-                    <ul className="mt-4 mb-1 space-y-4 font-serif text-xl text-justify">
-                        <li> - 3days and 2nights accommodation for 60pax with free breakfast (excess pax add. 800/per head)</li>
-                        <li> - Outdoor or Indoor Venue w/ basic Sound System</li>
-                        <li> - Basic Style with Tiffany Chair & Presidential Table</li>
-                        <li> - Free Photo shoot w/ 1 Changing room for whole day</li>
-                        <li> - 100pax food catering after ceremony</li>
-                        <li> - Free Food Tasting for 2-3person</li>
-                        <li> - Grazing Table</li>
-                        <li> - 2 to 3 hours use of Photo booth</li>
-                    </ul>
-                </div>
-        
-                {/* Wedding Package B */}
-                <div className="mr-25 mb-5 mt-5 p-6 text-gray-800 transition-colors duration-200 shadow-md inner-shadow  bg-[#A0B1B5] opacity-75 hover:bg-[#2F515B] hover:text-[#F2EFE8]">
-                    {/* Adjusted font size for package titles */}
-                    <h3 className="mb-4 font-serif text-3xl font-semibold">Wedding Package B</h3>
-                    <ul className="mt-4 mb-4 font-serif text-xl font-semibold">
-                    <p>Rate: 380,000</p>
-                    <p>Promo Rate: 350,000</p>
-                    </ul>
-                    <ul className="mt-4 mb-1 space-y-4 font-serif text-xl text-justify">
-                        <li> - 1 night accommodation for 60pax with free breakfast (excess pax add. 800/per head)</li>
-                        <li> - Indoor Venue w/ basic Sound System</li>
-                        <li> - Basic Style with Tiffany Chair & Presidential Table</li>
-                        <li> - Free Photo shoot w/ 1 Changing room for whole day.</li>
-                        <li> - 100pax food catering after ceremony</li>
-                        <li> - Free Food Tasting for 2-3person</li>
-                        <li> - Grazing Table</li>
-                        <li> - 2 to 3 hours use of Photo booth</li>
-                    </ul>
-                </div>
-
-
-            <div className="flex flex-col space-y-6">
-                <div className="flex space-x-10">
-                
-                {/* Wedding Package C */}
-                    <div className="h-auto mt-5 mr-10 p-6 text-gray-800 transition-colors duration-200 shadow-md inner-shadow bg-[#A0B1B5] opacity-75 hover:bg-[#2F515B] hover:text-[#F2EFE8]">
-                        <h3 className="mb-4 font-serif text-3xl font-semibold">Wedding Package C</h3>
-                            <ul className="mt-4 mb-4 font-serif text-lg font-semibold">
-                                <p>Rate: 200,000</p>
-                                <p>Promo Rate: 180,000</p>
-                            </ul>
-                                <ul className="mt-4 mb-1 space-y-2 font-serif text-lg text-justify">
-                                    <li> - 1 night room accommodation for 60pax with breakfast (excess pax add. 800/per head)</li>
-                                </ul>
-                    </div>
-                </div>
-        
-            
-                <div className="flex space-x-10">
-                    {/* Wedding Package D */}
-                    <div className="h-auto mr-10 p-6 text-gray-800 transition-colors duration-200 shadow-md inner-shadow bg-[#A0B1B5] opacity-75 hover:bg-[#2F515B] hover:text-[#F2EFE8]">
-                        <h3 className="mb-4 font-serif text-3xl font-semibold">Wedding Package D</h3>
-                            <ul className="mt-4 mb-4 font-serif text-lg font-semibold">
-                                <p>Rate: 75,000</p>
-                                <p>Promo Rate: 50,000</p>
-                            </ul>
-                                <ul className="mt-4 mb-1 space-y-2 font-serif text-lg text-justify">
-                                    <li> - Pavillion Venue                                    </li>
-                                    <li> - 50-80 pax</li>
-                                    <li> - 4 to 6 hours use</li>
-                                    <li> - With basic sound system & wi-fi access for all</li>
-                                    <li> - 1 room accommodation for 2 pax</li>
-                                </ul>
-                    </div>
-                </div>
-
-                <div className="flex space-x-10">
-                    {/* Wedding Package E */}
-                    <div className="h-auto mb-5 mr-10 p-6 text-gray-800 transition-colors duration-200 shadow-md inner-shadow bg-[#A0B1B5] opacity-75 hover:bg-[#2F515B] hover:text-[#F2EFE8]">
-                        <h3 className="mb-4 font-serif text-3xl font-semibold">Wedding Package E</h3>
-                            <ul className="mt-4 mb-4 font-serif text-lg font-semibold">
-                                <p>Rate: 300,000</p>
-                            </ul>
-                                <ul className="mt-4 mb-1 space-y-2 font-serif text-lg text-justify">
-                                    <li> - 2 nights accommodation for 60pax with free breakfast</li>
-                                    <li> - Indoor or Outdoor Ceremony & Reception</li>
-                                
-                                </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        <WeddingPackagesCarousel/>
 
     {/* 2nd section */}
     
-    <div className="mt-10 h-auto p-6 text-black transition-colors duration-200 shadow-md bg-[#CBA989] opacity-75">
+    <div className="h-auto p-6 mt-10 text-black transition-colors duration-200 shadow-md">
         
         <div className="my-6 text-center">
             <h2 className="mb-5 font-serif text-4xl text-black">Special Offers</h2>
