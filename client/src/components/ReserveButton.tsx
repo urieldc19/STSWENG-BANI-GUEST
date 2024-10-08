18b0be770375b8f2235d570b6fa05708920c95ff
joehannaC <jhnnacansino@gmail.com>
2024-10-08 19:46:59 +0800

    Update ReserveButton.tsx
    change color

--- client/src/components/ReserveButton.tsx
+++ client/src/components/ReserveButton.tsx
@@ -5,7 +5,7 @@ const ReserveNowButton = () => {
         <div className="my-6 text-center">
             <Link 
                 to="/reservations" 
-                className="mt-5 px-4 py-2 font-serif font-semibold bg-[#CBA989] text-black transition-colors duration-300 border border-black hover:bg-[#2F515B] hover:text-[#F2EFE8] inline-block"
+                className="mt-5 px-4 py-2 font-serif font-semibold text-black transition-colors duration-300 border border-black hover:bg-[#CBA989] hover:text-black inline-block"
             >
                 Reserve Now
             </Link>
