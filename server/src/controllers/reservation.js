f7a19a092ad99324f9e93544386dfed767d9a6f5
John Uriel Dela Cruz <136968627+urieldc19@users.noreply.github.com>
2024-10-08 13:53:43 +0800

    Update reservation.js

--- server/src/controllers/reservation.js
+++ server/src/controllers/reservation.js
@@ -1,4 +1,40 @@
-const { Client, Reservation } = require('./models/models.js')
+const { Client, Reservation } = require('../models/models.js')
+
+async function clientChecker (clientEntry) {
+
+    try {
+
+        const clientFirstName = clientEntry.firstName
+        const clientLastName = clientEntry.lastName
+        const clientAddress = clientEntry.address
+
+        const clientMatch = await Client.findOne({'firstName' : clientFirstName, 'lastName' : clientLastName, 'address' : clientAddress})
+
+        if (clientMatch) {
+
+            return clientMatch.clientId
+
+        } else {
+
+            const clientCount = await Client.countDocuments()
+
+            const newClientId = `C${String(clientCount + 1).padStart(8, '0')}`;
+
+            const newClientEntry = {
+                clientId: newClientId,
+                ...clientEntry
+            }
+
+            await Client.create(newClientEntry)
+
+            return newClientId
+        }
+
+    } catch (error) {
+        console.error(error)
+        return null
+    }
+}
 
 const createReservation = async (req, res) => {
 
@@ -22,7 +58,7 @@ const createReservation = async (req, res) => {
 
         const totalAmount = 0.00
         const amountPaid = 0.00
-        const clientId = "C001"
+        const arrivalStatus = "Booked"
 
         const clientEntry = {
             firstName,
@@ -32,9 +68,16 @@ const createReservation = async (req, res) => {
             address
         }
 
+        const clientId = await clientChecker(clientEntry)
+
+        const reservationCount = await Reservation.countDocuments()
+
+        const referenceNo = `R${String(reservationCount + 1).padStart(8, '0')}`;
+
         const reservationEntry = {
+            referenceNo,
             roomId,
-            roomId,
+            arrivalStatus,
             checkInDate, 
             checkOutDate, 
             numberOfAdults, 
@@ -47,13 +90,13 @@ const createReservation = async (req, res) => {
             clientId 
         }
 
-        await Client.create(clientEntry)
-
         await Reservation.create(reservationEntry)
 
         res.status(201).json({ message: "Reservation created successfully" });
 
     } catch (error) {
+
+        res.status(500).json({message: "Reservation creation failed"})
         console.error(error)
     }
 };
