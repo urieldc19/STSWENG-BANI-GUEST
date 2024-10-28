28b4e84f75d4246f96843f186cec51a9d3013171
Mary Faye Caya <146728425+mughriah@users.noreply.github.com>
2024-10-28 16:08:46 +0800

    Update ScenicBalconySuite.tsx

--- client/src/pages/ScenicBalconySuite.tsx
+++ client/src/pages/ScenicBalconySuite.tsx
@@ -1,5 +1,7 @@
 import { Slideshow } from '../components/Slideshow';
 import '../components/styles.css';
+import { Link } from 'react-router-dom';
+
 
 const coverImage = './images/room-cover/Scenic.png';
 
@@ -77,10 +79,12 @@ const ScenicBalconySuite = () => {
       </div>
 
       <div className="flex justify-center mt-6 mb-6">
-        <button className="bg-[#966732] text-white px-8 py-4 rounded text-lg font-semibold transition-transform duration-300 ease-in-out transform hover:bg-[#6b4c1e] hover:scale-105">
-          Book Now
-        </button>
-      </div>
+  <Link to="/reservations">
+    <button className="bg-[#966732] text-white px-8 py-4 rounded text-lg font-semibold transition-transform duration-300 ease-in-out transform hover:bg-[#6b4c1e] hover:scale-105">
+      Book Now
+    </button>
+  </Link>
+</div>
     </div>
   );
 };
