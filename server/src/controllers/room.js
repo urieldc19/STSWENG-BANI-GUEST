64a504dc145617e431e96421c04a14005c11274e
very-vivid-sky <cyrusrylie.bautista@gmail.com>
2024-11-22 17:41:21 +0800

    default images

--- server/src/controllers/room.js
+++ server/src/controllers/room.js
@@ -5,6 +5,19 @@ const PLACEHOLDER_IMAGES = [
     "./images/placeholder2.jpg",
     "./images/placeholder3.jpg",
 ]
+const ROOM_PLACEHOLDER_IMAGES = {
+    "Room 1": ["./public/images/family-bunk/fam-1.png", "./public/images/family-bunk/fam-2.png", "./public/images/family-bunk/fam-3.png", "./public/images/family-bunk/fam-4.png"],
+    "Room 2": ["./public/images/family-bunk/fam-1.png", "./public/images/family-bunk/fam-2.png", "./public/images/family-bunk/fam-3.png", "./public/images/family-bunk/fam-4.png"],
+    "Room 3": ["./public/images/family-bunk/fam-1.png", "./public/images/family-bunk/fam-2.png", "./public/images/family-bunk/fam-3.png", "./public/images/family-bunk/fam-4.png"],
+    "Room 4": ["./public/images/lovers-hideaway/lovers-1.png", "./public/images/lovers-hideaway/lovers-2.png", "./public/images/lovers-hideaway/lovers-3.png", "./public/images/lovers-hideaway/lovers-4.png"],
+    "Room 5": ["./public/images/lovers-hideaway/lovers-1.png", "./public/images/lovers-hideaway/lovers-2.png", "./public/images/lovers-hideaway/lovers-3.png", "./public/images/lovers-hideaway/lovers-4.png"],
+    "Room 6": ["./public/images/lovers-hideaway/lovers-1.png", "./public/images/lovers-hideaway/lovers-2.png", "./public/images/lovers-hideaway/lovers-3.png", "./public/images/lovers-hideaway/lovers-4.png"],
+    "Room 7": ["./public/images/scenic-balcony/scenic-1.png", "./public/images/scenic-balcony/scenic-2.png", "./public/images/scenic-balcony/scenic-3.png"],
+    "Room 7": ["./public/images/veranda-vista/veranda-1.png", "./public/images/veranda-vista/veranda-2.png", "./public/images/veranda-vista/veranda-3.png", "./public/images/veranda-vista/veranda-4.png"],
+    "Room 8": ["./public/images/luxe-suite/luxe-1.png", "./public/images/luxe-suite/luxe-2.png", "./public/images/luxe-suite/luxe-3.png", "./public/images/luxe-suite/luxe-4.png"],
+    "Villa": ["./public/images/serenity/serenity-1.png", "./public/images/serenity/serenity-2.png", "./public/images/serenity/serenity-3.png", "./public/images/serenity/serenity-4.png"],
+    "Hanging Kubo": ["./public/images/treetop-haven/treetop-1.png", "./public/images/treetop-haven/treetop-2.png", "./public/images/treetop-haven/treetop-3.png", "./public/images/treetop-haven/treetop-4.png"],
+}
 
 const getAvailableRooms = async (req, res) => {
 
@@ -32,12 +45,29 @@ const getRoomImages = async (req, res) => {
         let images = ""
 
         // check if undefined
+        /*
         if (data["images"] == undefined || data["images"] == "") {
-            images = PLACEHOLDER_IMAGES
+            if (reqRoomId in Object.keys(ROOM_PLACEHOLDER_IMAGES)) {
+                images = ROOM_PLACEHOLDER_IMAGES[reqRoomId];
+            } else {
+                images = PLACEHOLDER_IMAGES;
+            }
             console.log(`Retrieved placeholder for id ${reqRoomId}`)
         }  else {
             images = data["images"].split(",")
             console.log(`Retrieved images for id ${reqRoomId}`)
+        }
+        */
+        if (data["images"] != undefined && data["images"] != "") {
+            console.log(data)
+            images = data[images].split(",");
+            console.log(`Retrieved images for id ${reqRoomId}`)
+        } else if (Object.keys(ROOM_PLACEHOLDER_IMAGES).includes(reqRoomId)) {
+            images = ROOM_PLACEHOLDER_IMAGES[reqRoomId];
+            console.log(`Retrieved placeholder for id ${reqRoomId}`)
+        } else {
+            images = PLACEHOLDER_IMAGES;
+            console.log(`Retrieved placeholder for id ${reqRoomId}`)
         }
 
         res.json(images);
@@ -54,8 +84,10 @@ const getAllRoomImages = async (req, res) => {
         let images = await Room.find({}).select(["roomId", "images"])
         let imageJson = {};
         for (i of images) {
-            if (i.images != undefined || i.images == "") {
+            if (i.images != undefined && i.images != "") {
                 imageJson[i.roomId] = i.images.split(",");
+            } else if (Object.keys(ROOM_PLACEHOLDER_IMAGES).includes(i.roomId)) {
+                imageJson[i.roomId] = ROOM_PLACEHOLDER_IMAGES[i.roomId];
             } else {
                 imageJson[i.roomId] = PLACEHOLDER_IMAGES;
             }
